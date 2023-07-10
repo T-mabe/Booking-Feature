fa76d47f699e5eea38a932de47098356ad4f08c1
Abe Molina <abemolina03@gmail.com>
2023-07-10 12:39:47 -0400

    Initial Commit

---
+++ ClickUITest/ClickUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickUITestApp.swift
+//  ClickUITest
+//
+//  Created by Abe Molina on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ClickUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClickUITest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ClickUITest
+//
+//  Created by Abe Molina on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
